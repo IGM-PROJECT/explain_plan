8b894bbf379e049fdcf1aad11d9892f2af0b01cc
mario robayo <marioferyhwh@gmail.com>
2021-04-02 12:14:47 -0500

    se hace posible subir archivos con  la configuracion

--- src/app/module/graph-explain-plan/graph-explain-plan.component.ts
+++ src/app/module/graph-explain-plan/graph-explain-plan.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 import { ExplainObjService } from "src/app/service/explain-obj.service";
-import { PurpleAttribute, Root,ExplainPlanObj } from "src/app/shared/model/explain-plan-obj";
+import {
+  PurpleAttribute,
+  Root,
+  ExplainPlanObj,
+} from "src/app/shared/model/explain-plan-obj";
 
 @Component({
   selector: "app-graph-explain-plan",
@@ -12,10 +16,21 @@ export class GraphExplainPlanComponent implements OnInit {
   constructor(private _planService: ExplainObjService) {}
 
   ngOnInit(): void {
-    this.explain_pla = this._planService.getExplainPlan();
+    // this.explain_pla = this._planService.getExplainPlan();
     //console.log(this.explain_pla.root[0])
   }
+  clearData() {
+    this.explain_pla = null;
+  }
 
-
-
+  cargarArchivo(ev: Event) {
+    //console.log(ev);
+    let file = ev.target["files"][0];
+    var readFile = new FileReader();
+    readFile.onload =  (e)=> {
+      let contents: string = String(e.target.result);
+      this.explain_pla= JSON.parse(contents);
+    };
+    readFile.readAsText(file);
+  }
 }
